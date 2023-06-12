b469833a7c8f403bbc79850b3b74034bd7d5d682
Balkaran Singh Aujla <98044007+Balkaran1720@users.noreply.github.com>
2023-06-12 18:40:01 -0400

    Update tic1.js

--- tic-tac-toe/tic1.js
+++ tic-tac-toe/tic1.js
@@ -1,20 +1,107 @@
+//getting element by using className
 const commonBtn = document.getElementsByClassName("tictactoe-btn");
-for (let i = 0; i < commonBtn.length; i++) {
-  let btn = commonBtn[i];
-  btn.innerHTML = "";
-  btn.addEventListener("click", () => {
-    userClickedBtnIndex(btn, i);
-    checkAll();
-  });
+
+initialize();
+//making function to initialze the code
+function initialize() {
+  //looping over the clicked buttons
+  for (let i = 0; i < commonBtn.length; i++) {
+    let btn = commonBtn[i];
+    btn.innerHTML = "";
+    btn.addEventListener("click", () => {
+      userClickedBtnIndex(btn, i);
+      checkAll();
+    });
+  }
 }
 
+//checking if user clicked the btn
 function userClickedBtnIndex(btn, index) {
   if (btn.innerHTML == "") {
     btn.innerHTML = "x";
+    // solveSystemMoveDumb();
+
+    solveSystemMoveSmart();
+    //checkWinningCombo(i, j, k);
+  }
+}
+
+/**
+ * Finds optimal move
+ */
+function solveSystemMoveSmart() {
+  let computerIndex = checkIfWinningCombinationExists("o");
+  let playerIndex = checkIfWinningCombinationExists("x");
+  if (computerIndex > -1) {
+    commonBtn[computerIndex].innerHTML = "o";
+  } else if (playerIndex > -1) {
+    commonBtn[playerIndex].innerHTML = "o";
+  } else {
     solveSystemMoveDumb();
   }
 }
 
+/**
+ * Check all valid combinations and find the winning index.
+ * If no index is found return -1
+ * @param {*} symbol
+ */
+function checkIfWinningCombinationExists(symbol) {
+  let check = checkCombinationToWin(0, 1, 2, symbol);
+  if (check > -1) return check;
+
+  check = checkCombinationToWin(3, 4, 5, symbol);
+  if (check > -1) return check;
+
+  check = checkCombinationToWin(6, 7, 8, symbol);
+  if (check > -1) return check;
+
+  check = checkCombinationToWin(0, 3, 6, symbol);
+  if (check > -1) return check;
+
+  check = checkCombinationToWin(1, 4, 7, symbol);
+  if (check > -1) return check;
+
+  check = checkCombinationToWin(2, 5, 8, symbol);
+  if (check > -1) return check;
+
+  check = checkCombinationToWin(0, 4, 8, symbol);
+  if (check > -1) return check;
+
+  check = checkCombinationToWin(2, 4, 6, symbol);
+  if (check > -1) return check;
+  return -1;
+}
+
+/**
+ * Function checks i, j and k to see if one of these location contains the winning move for symbol
+ * @param {*} i
+ * @param {*} j
+ * @param {*} k
+ * @param {*} symbol
+ * @returns
+ */
+function checkCombinationToWin(i, j, k, symbol) {
+  if (commonBtn[i].innerHTML == symbol && 
+      commonBtn[j].innerHTML == symbol && 
+      commonBtn[k].innerHTML == "")
+    return k;
+  else if (
+    commonBtn[i].innerHTML == symbol &&
+    commonBtn[j].innerHTML == "" &&
+    commonBtn[k].innerHTML == symbol
+  )
+    return j;
+  else if (
+    commonBtn[i].innerHTML == "" &&
+    commonBtn[j].innerHTML == symbol &&
+    commonBtn[k].innerHTML == symbol
+  )
+    return i;
+  else return -1;
+}
+
+//making a function for AI to make possible moves
 function solveSystemMoveDumb() {
   for (let i = 0; i < commonBtn.length; i++) {
     let btn = commonBtn[i];
@@ -25,6 +112,7 @@ function solveSystemMoveDumb() {
   }
 }
 
+// checking if the game has finished
 function checkCombo(symbol) {
   if (
     combinations(0, 1, 2, symbol) ||
@@ -39,9 +127,8 @@ function checkCombo(symbol) {
     alert(`player ${symbol} has won the game`);
     reset();
   }
-
 }
-
+//applying boolean to see combo
 function combinations(i, j, k, symbol) {
   if (
     commonBtn[i].innerHTML === symbol &&
@@ -49,19 +136,18 @@ function combinations(i, j, k, symbol) {
     commonBtn[k].innerHTML === symbol
   ) {
     return true;
-    // alert(`player ${symbol} has won the game`);
   } else {
     return false;
   }
 }
-
+//reset after the game has finished
 function reset() {
   for (let i = 0; i < commonBtn.length; i++) {
     const btn = commonBtn[i];
     btn.innerHTML = "";
   }
 }
-
+//checking both the combo for 'x' and 'o'
 function checkAll() {
   checkCombo("x");
   checkCombo("o");

162e9f344e37dbc7ccc6ed81fd61e38ac122a79d
Balkaran Singh Aujla <98044007+Balkaran1720@users.noreply.github.com>
2023-06-14 23:35:18 -0400

    battleship.js

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -1,6 +1,15 @@
-let boardSize = 10;
-let shipSizeArray = [7, 5, 3, 4, 2];
+let boardSize;
+let number;
+let shipSizeArray = [3, 2];
+let selectedGrid =[];
+let userClicked = 0;
 let body = document.getElementsByTagName("body")[0];
+
+do {
+  boardSize = prompt("Select the playing board size(5, 10, 15, or 20):");
+  number = parseInt(boardSize);
+} while (isNaN(number) || ![5, 10, 15, 20].includes(number));
+
 for (let row = 0; row < boardSize; row++) {
   let divRow = document.createElement("div");
   divRow.className = "row";
@@ -8,6 +17,23 @@ for (let row = 0; row < boardSize; row++) {
   for (let column = 0; column < boardSize; column++) {
     let divColumn = document.createElement("div");
     divColumn.className = "column";
-    body.appendChild(divColumn);
+    divRow.appendChild(divColumn);
+
+    divColumn.addEventListener("click", function () {
+      const position = row * boardSize + column;
+
+      for (let i = 0; i < shipSizeArray.length; i++) {
+        if (!isSelected(position) && userClicked[i] < shipSizeArray[i]) {
+          divColumn.style.backgroundColor = "red";
+          console.log("grid", position, "clicked");
+          userClicked++;
+          selectedGrid.push(position);
+        }
+      }
+    });
   }
+}
+
+function isSelected(position) {
+  return selectedGrid.includes(position);
 }
